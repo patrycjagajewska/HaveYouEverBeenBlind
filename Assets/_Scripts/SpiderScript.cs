c56cfa9032fa0e97c457d561b4f221428b8fe9df
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2017-09-13 00:00:22 +0200

    Scary spiders on their way!

---
+++ Assets/_Scripts/SpiderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderScript : MonoBehaviour {
+
+    Animator anim;
+
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
