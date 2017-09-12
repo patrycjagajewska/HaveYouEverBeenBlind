d17556100a5de35210f44cbfa884d037477cf0a6
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2017-09-12 22:25:19 +0200

    Add lightning effects to level 1

---
+++ Assets/_Scripts/AutoBlinker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBlinker : MonoBehaviour {
+
+    public float duration = 2.0f;
+    
+    public float intensity = 1f;
+
+    private Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+    void Update()
+    {
+        float phi = Time.time / duration * 2 * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * intensity + 2 * intensity;
+        lt.intensity = amplitude;
+    }
+}
---
+++ Assets/_Scripts/AutoSpinner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSpinner : MonoBehaviour {
+
+    public float speed = 100;
+
+    void Update() {
+        transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
