4471df98690edc5950f229c84aef24bad14cb7b1
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2017-09-15 01:37:55 +0200

    Waterball improvements across levels

--- Assets/_Scripts/TouchController.cs
+++ Assets/_Scripts/TouchController.cs
@@ -15,16 +15,5 @@ public class TouchController : MonoBehaviour {
         rigidbody.velocity = new Vector3(0, 0, 0);
         rigidbody.useGravity = false;
         Destroy(gameObject, 1);
-        /*
-        GameObject lightGameObject = new GameObject("touch-light");
-        Light lightComp = lightGameObject.AddComponent<Light>();
-        lightComp.color = Color.blue;
-        lightComp.areaSize = new Vector2(1f, 1f);
-        var x = col.gameObject.transform.position.x;
-        var y = col.gameObject.transform.position.y;
-        var z = col.gameObject.transform.position.z;
-        lightGameObject.transform.position = new Vector3(x, y, z);
-        Destroy(col.gameObject);*/
-        
     }
 }
