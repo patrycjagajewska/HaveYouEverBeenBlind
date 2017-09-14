a32b6465074c72879d6e04b53bd7e4df9cc9c235
pkoniu <patryk.konior@gmail.com>
2017-09-15 00:36:35 +0200

    touching simulation added

---
+++ Assets/_Scripts/TouchController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        GameObject lightGameObject = new GameObject("touch-light");
+        Light lightComp = lightGameObject.AddComponent<Light>();
+        lightComp.color = Color.blue;
+        var x = col.gameObject.transform.position.x;
+        var y = col.gameObject.transform.position.y;
+        var z = col.gameObject.transform.position.z;
+        lightGameObject.transform.position = new Vector3(x, y, z);
+        Destroy(col.gameObject);
+    }
+}
---
+++ Assets/_Scripts/TouchLightSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchLightSpawner : MonoBehaviour
+{
+
+    public Transform prefab;
+    public float speed = 20;
+    public float fireRate = 0.2F;
+    private float nextFire = 0.0F;
+    public float destructionTime = 0.05f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            shoot();
+        }
+    }
+
+    public void shoot()
+    {
+        var position = transform.position;
+
+        Transform bullet = Instantiate(prefab, position, Random.rotation);
+        Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * speed;
+
+        Destroy(bullet.gameObject, destructionTime);
+    }
+}
