696f491bd9678aba483dbdccc3bd9a0b8e1fa4ba
Patrycja Wrona <patrycjawrona23@gmail.com>
2017-09-15 02:44:12 +0200

    Merge conflict - resolved conflicts by hand

---
+++ Assets/_Scripts/Instructions1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Instructions1 : MonoBehaviour {
+
+	public GameObject background;
+	public GameObject instructions;
+	public GameObject play;
+
+	public Button playButton;
+
+	// Use this for initialization
+	IEnumerator Start () {
+
+		background.SetActive (false);
+		instructions.SetActive (false);
+		play.SetActive (false);
+
+		yield return new WaitForSeconds (1);
+
+		background.SetActive (true);
+		instructions.SetActive (true);
+		play.SetActive (true);
+
+		playButton.onClick.AddListener (returnToGame);
+	}
+
+	void returnToGame() {
+		background.SetActive (false);
+		instructions.SetActive (false);
+		play.SetActive (false);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
